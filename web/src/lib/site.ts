6775fe41743e3cf405080ba5e38ec84610a1af0d
wlreich <wlreich@gmail.com>
2026-09-16 13:48:22 -0500

    Add live support payment URL

--- web/src/lib/site.ts
+++ web/src/lib/site.ts
@@ -2,6 +2,8 @@ export const SITE_OPERATOR = "Systems & Sense LLC";
 export const CONTACT_EMAIL = "hello@parcelsavvy.org";
 export const SITE_URL =
   process.env.NEXT_PUBLIC_SITE_URL ?? "https://property-tax-helper.vercel.app";
+export const SUPPORT_PAYMENT_URL =
+  "https://donate.stripe.com/6oUaEP7jn6l39fSgXg7AI00";
 export const ADJUSTMENT_METHOD_VERSION = "PS-ADJ-2026.1";
 export const NEIGHBORHOOD_METHOD_VERSION = "PS-NBR-2026.1";
 
