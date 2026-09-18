0bd1f43572f15935922443e5ecb080545b6194fb
wlreich <wlreich@gmail.com>
2026-09-18 17:57:44 -0500

    Centralize district identity for the home and search experience

---
+++ web/src/lib/appraisal-district.ts
@@ -0,0 +1,8 @@
+// Identity used by the home/search experience. Other counties require their
+// own supported data sources; changing this label alone does not add coverage.
+export const appraisalDistrict = {
+  countyLabel: "TRAVIS COUNTY, TEXAS",
+  name: "Travis Central Appraisal District",
+  abbreviation: "TCAD",
+  propertySearchUrl: "https://traviscad.org/propertysearch/",
+} as const;
